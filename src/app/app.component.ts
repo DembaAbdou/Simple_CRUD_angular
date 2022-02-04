c6d7f5e14cd86bce4f3278bcb00522194bc84665
DembaAbdou <senedemba98@gmail.com>
2022-02-04 21:07:17 +0100

    L'implémentation de la classe OnInit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+//import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { HttpClient} from '@angular/common/http';
 import { FormBuilder, FormGroup, Validators} from '@angular/forms';
@@ -8,6 +9,29 @@ import { FormBuilder, FormGroup, Validators} from '@angular/forms';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Simple_CRUD_Angular';
+
+  baseURL = 'http://rental.test/api/'
+  brandForm!: FormGroup;
+  isSubmitted = false;
+  brandList: any;
+
+  constructor(
+  private _http: HttpClient,
+  private _fb: FormBuilder
+  ) {}
+
+  ngOnInit() {
+    this.brandForm = this._fb.group({
+      id: [0],
+      name: ['', Validators.required],
+      is_active: [1] ,
+    });
+
+  }
+
+  get _fc() { return this.brandForm.controls;}
+
+
 }
